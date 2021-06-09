c304153f451c9b4095f147a7fcd58e53c5f5beae
dave <dave@wigwok.com>
2021-06-09 13:30:01 +0000

    added AGPL distinction on ERC contract

--- erc/contracts/Token.sol
+++ erc/contracts/Token.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 // Solidity files have to start with this pragma.
 // It will be used by the Solidity compiler to validate its version.
 pragma solidity ^0.7.0;
